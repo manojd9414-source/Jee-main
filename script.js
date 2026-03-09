ffa7052d88f11f0c9397eeafb7ce8f0b327df2e3
manojd9414-source <manojd9414@gmail.com>
2026-03-09 22:16:07 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+function calculateScore(){
+
+let n=parseInt(document.getElementById("qCount").value)
+
+let score=0
+let attempted=0
+let correct=0
+let wrong=0
+let unattempted=0
+
+for(let i=0;i<n;i++){
+
+let type=document.getElementById("type"+i).value
+let correctAns=document.getElementById("key"+i).value.trim()
+
+let user=""
+
+if(type=="MCQ"){
+
+let r=document.querySelector(`input[name=q${i}]:checked`)
+if(r) user=r.value
+
+if(user==""){
+unattempted++
+continue
+}
+
+attempted++
+
+if(user==correctAns){
+
+score+=4
+correct++
+
+}else{
+
+score-=1
+wrong++
+
+}
+
+}
+
+else{
+
+user=document.getElementById("num"+i).value.trim()
+
+if(user==""){
+unattempted++
+continue
+}
+
+attempted++
+
+if(user==correctAns){
+
+score+=4
+correct++
+
+}else{
+
+wrong++
+
+}
+
+}
+
+}
+
+let accuracy = attempted>0 ? ((correct/attempted)*100).toFixed(2) : 0
+
+let timePerQuestion = attempted>0 ? Math.floor((1200-time)/attempted) : 0
+
+let min=Math.floor(timePerQuestion/60)
+let sec=timePerQuestion%60
+
+document.getElementById("result").classList.remove("hidden")
+
+document.getElementById("result").innerHTML=`
+
+<h2 style="color:#00d4ff;font-family:Orbitron;">Score : ${score}</h2>
+
+<p>Attempted : ${attempted}</p>
+
+<p style="color:#00ff9d;">Correct : ${correct}</p>
+
+<p style="color:#ff6b35;">Wrong : ${wrong}</p>
+
+<p>Unattempted : ${unattempted}</p>
+
+<hr>
+
+<p>Accuracy : ${accuracy}%</p>
+
+<p>Avg Time / Question : ${min}m ${sec}s</p>
+
+`
+
+}
